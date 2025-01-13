d60154a685e3f9a6e330ca8c76cda983a0f47cab
emddeveloper <emd.developer@gmail.com>
2025-01-13 18:33:56 +0530

    first

---
+++ js/custom.js
@@ -0,0 +1,96 @@
+(function ($) {
+  "use strict";
+
+ 
+
+
+
+  
+  $(document).ready(function() {
+    var review = $('.textimonial_iner');
+  if (review.length) {
+    review.owlCarousel({
+      items: 1,
+      loop: true,
+      dots: true,
+      autoplay: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 5000,
+      nav: false,
+      responsive: {
+        0: {
+          margin: 15,
+        },
+        600: {
+          margin: 10,
+        },
+        1000: {
+          margin: 10,
+        }
+      }
+    });
+  }
+  });
+
+  $(document).ready(function() {
+    var review = $('.textimonial_iner_about');
+  if (review.length) {
+    review.owlCarousel({
+      items: 1,
+      loop: true,
+      dots: true,
+      autoplay: true,
+      autoplayHoverPause: true,
+      autoplayTimeout: 4000,
+      nav: false,
+      responsive: {
+        0: {
+          margin: 15,
+        },
+        600: {
+          margin: 10,
+        },
+        1000: {
+          margin: 10,
+        }
+      }
+    });
+  }
+  });
+  // menu fixed js code
+  $(window).scroll(function () {
+    var window_top = $(window).scrollTop() + 1;
+    if (window_top > 50) {
+      $('.main_menu').addClass('menu_fixed animated fadeInDown');
+    } else {
+      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
+    }
+  });
+
+
+ 
+  //remove active class from all thumbnail slides
+  $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+ 
+  //set active class to first thumbnail slides
+  $('.slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
+ 
+  // On before slide change match active thumbnail to current slide
+  $('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    var mySlideNumber = nextSlide;
+    $('.slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+    $('.slider-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+ });
+ 
+ //UPDATED 
+   
+ $('.slider').on('afterChange', function(event, slick, currentSlide){   
+   $('.content').hide();
+   $('.content[data-id=' + (currentSlide + 1) + ']').show();
+ }); 
+
+
+
+
+
+}(jQuery));
